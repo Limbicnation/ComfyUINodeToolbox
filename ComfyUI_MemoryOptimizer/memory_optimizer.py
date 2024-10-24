59376bff84a0814bb3e0e25a8dd4f24f9e9aacd2
limbicnation <gero@limbicnation.com>
2024-10-25 01:51:17 +0200

    feat(memory-optimizer): add toggle switch for aspect ratio preservation
    Added a new boolean parameter 'keep_aspect_ratio' to Memory Optimizer node that allows:
    
    Maintaining original image aspect ratio (default: true)
    Forcing square dimensions when set to false
    Resizing behavior now controlled by user preference

--- ComfyUI_MemoryOptimizer/memory_optimizer.py
+++ ComfyUI_MemoryOptimizer/memory_optimizer.py
@@ -14,6 +14,7 @@ class MemoryOptimizer:
                     "step": 64
                 }),
                 "use_half_precision": ("BOOLEAN", {"default": True}),
+                "keep_aspect_ratio": ("BOOLEAN", {"default": True}),
             },
         }
 
@@ -21,20 +22,25 @@ class MemoryOptimizer:
     FUNCTION = "optimize_memory"
     CATEGORY = "utils"
 
-    def optimize_memory(self, image, max_resolution, use_half_precision):
+    def optimize_memory(self, image, max_resolution, use_half_precision, keep_aspect_ratio):
         # Ensure input is a torch tensor
         if not isinstance(image, torch.Tensor):
             image = torch.tensor(image)
 
         # Resize image if necessary
         if image.shape[-1] > max_resolution or image.shape[-2] > max_resolution:
-            aspect_ratio = image.shape[-2] / image.shape[-1]
-            if aspect_ratio > 1:
-                new_height = max_resolution
-                new_width = int(max_resolution / aspect_ratio)
+            if keep_aspect_ratio:
+                aspect_ratio = image.shape[-2] / image.shape[-1]
+                if aspect_ratio > 1:
+                    new_height = max_resolution
+                    new_width = int(max_resolution / aspect_ratio)
+                else:
+                    new_width = max_resolution
+                    new_height = int(max_resolution * aspect_ratio)
             else:
+                new_height = max_resolution
                 new_width = max_resolution
-                new_height = int(max_resolution * aspect_ratio)
+            
             image = torch.nn.functional.interpolate(image, size=(new_height, new_width), mode='bilinear', align_corners=False)
 
         # Convert to half precision if requested
@@ -51,5 +57,5 @@ class MemoryOptimizer:
         return (image,)
 
     @classmethod
-    def IS_CHANGED(s, image, max_resolution, use_half_precision):
+    def IS_CHANGED(s, image, max_resolution, use_half_precision, keep_aspect_ratio):
         return float("NaN")
